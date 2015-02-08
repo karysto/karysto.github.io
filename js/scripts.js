3ca9f7ac05779a8f1870993adf6e2d377b1a2b88
Thanasi <tomkay94@gmail.com>
2015-02-08 01:14:04 -0500

    Refactor and isolate js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,32 @@
-$("#loader").show();
-  $("#wholething").hide();
-  $(window).load(function() {
-      $("#loader").hide();
-      $("#wholething").show();
+// Scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
-  $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html,body').animate({
-                  scrollTop: target.offset().top
-              }, 1000);
-              return false;
-            }
-        }
-      });
-  });
+});
 
-  $(function() {
-    document.getElementById('em').innerHTML =
-    document.getElementById('em').innerHTML.replace(/#_AT_#/g, '@').replace(/#_DOT_#/g, '.');
-  });
+// Email protection
+$(function() {
+  document.getElementById('em').innerHTML =
+  document.getElementById('em').innerHTML.replace(/#_AT_#/g, '@').replace(/#_DOT_#/g, '.');
+});
+
+// Active nav tab
+$(".navitem").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
