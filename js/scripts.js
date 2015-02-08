1fec7e5350b46cfd2003b0db58785f1a589db546
Thanasi <tomkay94@gmail.com>
2015-02-08 00:50:26 -0500

    Isolate scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
-ul li:hover a{
-  background: #555;
-  color: #fff;
-}
+$("#loader").show();
+  $("#wholething").hide();
+  $(window).load(function() {
+      $("#loader").hide();
+      $("#wholething").show();
+  });
+  $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html,body').animate({
+                  scrollTop: target.offset().top
+              }, 1000);
+              return false;
+            }
+        }
+      });
+  });
 
-ul a:hover {
-  background: #555;
-  color: #fff;
-}
+  $(function() {
+    document.getElementById('em').innerHTML =
+    document.getElementById('em').innerHTML.replace(/#_AT_#/g, '@').replace(/#_DOT_#/g, '.');
+  });
